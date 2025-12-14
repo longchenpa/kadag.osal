36544da4ceef39c40f37e75e2da50a144291343b
5HT <maxim@synrc.com>
2025-12-14 07:18:47 +0000

    wip

---
+++ shog.dril.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+cat "dkar.chag.txt" > shog.dril.txt
+cat "smon.lam.txt" >> shog.dril.txt
+cat "sngon.'gro" >> shog.dril.txt
+cat "khrus.txt" >> shog.dril.txt
+cat "dge.bsngo.txt" >> shog.dril.txt
+cat "rnam.thar.txt" >> shog.dril.txt
+cat "lo.rgyud.txt" >> shog.dril.txt
+cat "sems.phyogs.txt" >> shog.dril.txt
+cat "chos.mngon.pa.txt" >> shog.dril.txt
+cat "dbu.ma.txt" >> shog.dril.txt
+cat "rnal.'byor.spyod.pa.txt" >> shog.dril.txt
+cat "tshad.ma.txt" >> shog.dril.txt
+cat "rgyud.txt" >> shog.dril.txt
+cat "dbang" >> shog.dril.txt
+cat "bskang.ba.txt" >> shog.dril.txt
+cat "mchod.pa.txt" >> shog.dril.txt
+cat "sngags.txt" >> shog.dril.txt
+cat "dkyil.'khor.txt" >> shog.dril.txt
+cat "tshogs.txt" >> shog.dril.txt
+cat "chod.txt" >> shog.dril.txt
+cat "sbyin.sreg.txt" >> shog.dril.txt
+cat "sgrub.thabs.txt" >> shog.dril.txt
+cat "lha.srung.txt" >> shog.dril.txt
+cat "lha.txt" >> shog.dril.txt
+cat "bka'.brgyad.txt" >> shog.dril.txt
+cat "zhi khro.txt" >> shog.dril.txt
+cat "tshe.sgrub.txt" >> shog.dril.txt
+cat "phyi.bla.ma.rnal.'byor.txt" >> shog.dril.txt
+cat "bla.ma.rnal.'byor.txt" >> shog.dril.txt
+cat "'pho.ba.txt" >> shog.dril.txt
+cat "gtum.mo.txt" >> shog.dril.txt
+cat "rtsa.rlung.thig.txt" >> shog.dril.txt
+cat "nang.gsang.rnal.'byor.txt" >> shog.dril.txt
+cat "sems.sde'i.rnal.'byor.bzhi.txt" >> shog.dril.txt
+cat "khregs.chod.txt" >> shog.dril.txt
+cat "thod.rgal.txt" >> shog.dril.txt
+cat "'od.gsal.txt" >> shog.dril.txt
+cat "bar.do.txt" >> shog.dril.txt
+cat "yang.ti.txt" >> shog.dril.txt
+
